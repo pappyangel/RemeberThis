badfc0d064ad873b7ca246596bf9501c95adc506
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2023-02-07 17:54:30 +0000

    working b2c blaz app

--- BlazB2C/Program.cs
+++ BlazB2C/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddAuthorization(options =>
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor()
     .AddMicrosoftIdentityConsentHandler();
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
@@ -34,9 +35,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+
 
 app.UseStaticFiles();
 
