d64f214941344eb1fec1b66be899d62f9922949b
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2023-02-12 01:07:00 +0000

    Blazor passing class data and file to API

--- BlazorWebApp/Pages/AddRTBase.cs
+++ BlazorWebApp/Pages/AddRTBase.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Components.Forms;
 using SharedModels;
 using Microsoft.JSInterop;
 using System.Net.Http;
+using System.Text.Json;
+using System.Net.Http.Headers;
 
 namespace BlazorWebApp.Pages
 {
@@ -10,7 +12,7 @@ namespace BlazorWebApp.Pages
     public class AddRTBase : ComponentBase
     {
         protected IBrowserFile? file;
-        protected string? myAPIMessage { get; set; }
+        protected string? myAPIMessage { get; set; } = "API Return Message";
         protected string apiBase { get; set; } = "http://127.0.0.1:5026";
 
         protected string apiRoute { get; set; } = "/RememberThis/rtMulti";
@@ -34,25 +36,64 @@ namespace BlazorWebApp.Pages
         [Inject]
         protected IHttpClientFactory ClientFactory { get; set; }
 
-      
+
         protected async Task SelectedFileProcess(InputFileChangeEventArgs e)
-        {            
+        {
             file = e.File;
             await jsRuntime.InvokeVoidAsync("loadFileJS");
         }
         protected async Task SubmitForm()
         {
             ShowPopUp = true;
-            int dog = 0;
-            
+
+
             using var content = new MultipartFormDataContent();
             var client = ClientFactory.CreateClient();
-            var response =  await client.PostAsync("http://127.0.0.1:5197/RememberThis/Blaz",  content);
-            
-            dog++;
+
+            //Add form data that bound to class into jsaon string via serialization
+            string jsonString = JsonSerializer.Serialize(thisrtItem);
+            var classContent = new StringContent(jsonString);
+            content.Add(classContent, "classData");
+
+            //Add file 
+            var ms = new MemoryStream();
+            await file.OpenReadStream().CopyToAsync(ms);
+            var streamContent = new StreamContent(ms);
+            streamContent.Headers.ContentType = MediaTypeHeaderValue.Parse(file.ContentType);
+
+            content.Add(streamContent, "file", file.Name);
+
+
+            var response1 = await client.PostAsync("http://127.0.0.1:5197/RememberThis/Blaz", content);
+
+            switch (response1.StatusCode)
+            {
+                case System.Net.HttpStatusCode.OK:
+                    myAPIMessage = await response1.Content.ReadAsStringAsync();
+                    break;
+                case System.Net.HttpStatusCode.NoContent:
+                    myAPIMessage = "No content";
+                    break;
+                case System.Net.HttpStatusCode.NotFound:
+                    myAPIMessage = "API Route not found!";
+                    break;
+                case System.Net.HttpStatusCode.Forbidden:
+                    myAPIMessage = "Your Access to this API route is Forbidden!";
+                    break;
+                case System.Net.HttpStatusCode.Unauthorized:
+                    myAPIMessage = "Your Access to this API route is Unauthorized!";
+                    break;
+                default:
+                    myAPIMessage = "Unhandled Error!";
+                    break;
+
+            }
+
+
+
 
         }
-        
+
     }
 
 }
--- RememberThis/Controllers/RememberThisController.cs
+++ RememberThis/Controllers/RememberThisController.cs
@@ -22,13 +22,13 @@ public class RememberThisController : ControllerBase
     [HttpGet(Name = "GetRememberThis")]
     public ActionResult<rtItem> Get()
     {
-        rtItem getItem = new rtItem{ rtId = 1001, rtUserName = "Cosmo", rtDescription = "fun time digging hole for bone",rtLocation = "backyard", rtDateTime =  DateTime.UtcNow };
-      
+        rtItem getItem = new rtItem { rtId = 1001, rtUserName = "Cosmo", rtDescription = "fun time digging hole for bone", rtLocation = "backyard", rtDateTime = DateTime.UtcNow };
+
         return Ok(getItem);
     }
 
     [HttpPost]
-    public ActionResult<rtItem> RememberThisUpload (rtItem _rtItem)
+    public ActionResult<rtItem> RememberThisUpload(rtItem _rtItem)
     {
         // HttpRequest request
         HttpRequest multipartRequest = HttpContext.Request;
@@ -36,26 +36,26 @@ public class RememberThisController : ControllerBase
         // var form = await request.ReadFormAsync();
 
         // string? jsonData = multipartRequest.Form["jsonData"];
-        
+
         //rtItem myItem = new();
 
         // rtItem myItem = JsonSerializer.Deserialize<rtItem>(jsonData);
 
         _logger.LogInformation("RememberThisUpload  {DT}",
             DateTime.UtcNow.ToLongTimeString());
-        
+
         // LocalTestMethod();
 
         return Ok(_rtItem);
 
     }
 
-[HttpPost]
-[Route("[action]")]
-    public ActionResult<rtItem> rtMulti ()
-    {           
-        HttpRequest multipartRequest = HttpContext.Request;  
-        
+    [HttpPost]
+    [Route("[action]")]
+    public ActionResult<rtItem> rtMulti()
+    {
+        HttpRequest multipartRequest = HttpContext.Request;
+
         StringValues rtItemJson;
         multipartRequest.Form.TryGetValue("classdata", out rtItemJson);
 
@@ -66,23 +66,23 @@ public class RememberThisController : ControllerBase
         return Ok(rtItemFromPost);
     }
 
-[HttpPost]
-[Route("[action]")]
-    public ActionResult<rtItem> Blaz ()
-    {           
-        HttpRequest multipartRequest = HttpContext.Request;  
-        
-        // StringValues rtItemJson;
-        // multipartRequest.Form.TryGetValue("classdata", out rtItemJson);
+    [HttpPost]
+    [Route("[action]")]
+    public ActionResult<rtItem> Blaz()
+    {
+        HttpRequest multipartRequest = HttpContext.Request;
 
-        // rtItem? rtItemFromPost = JsonSerializer.Deserialize<rtItem>(rtItemJson[0]);
+        StringValues rtItemJson;
+        multipartRequest.Form.TryGetValue("classdata", out rtItemJson);
 
-        // rtItemFromPost.rtImagePath = multipartRequest.Form.Files["file"].FileName.ToString();
+        rtItem? rtItemFromPost = JsonSerializer.Deserialize<rtItem>(rtItemJson[0]);
+
+        rtItemFromPost.rtImagePath = multipartRequest.Form.Files["file"].FileName.ToString();
 
         return Ok("Hello from Blaz Controller");
     }
 
-   
+
     //  public void LocalTestMethod()
     //     {       
     //         // test method demontrating calling a method in the same class file
