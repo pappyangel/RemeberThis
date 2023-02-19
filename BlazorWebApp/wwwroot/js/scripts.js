7616a431a70aae72fbc760c096b4882ae9d1e11f
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2023-02-19 15:35:18 +0000

    Cleaned Page names & started popover

--- BlazorWebApp/wwwroot/js/scripts.js
+++ BlazorWebApp/wwwroot/js/scripts.js
@@ -1,6 +1,16 @@
 
+// const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+// const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
 
+// var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+//     container: 'body'
+//   })
+
+//   var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+// var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+//   return new bootstrap.Popover(popoverTriggerEl)
+// })
 
 var loadFileJS = function () {    
     
@@ -9,3 +19,13 @@ var loadFileJS = function () {
     image.src = URL.createObjectURL(selectedFile);
 
 };
+
+// $(function () {
+//     $('[data-bs-toggle="popover"]').popover()
+//   })
+
+// $(document).ready(function() {
+//     $('[data-bs-toggle=popover]').popover();
+// }); 
+
+
