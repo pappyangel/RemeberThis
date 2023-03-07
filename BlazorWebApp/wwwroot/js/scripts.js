9d59da353ad0023d538b03ec0644196fbbd175a0
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2023-03-07 02:35:27 +0000

    PoC for SAS and Stream

--- BlazorWebApp/wwwroot/js/scripts.js
+++ BlazorWebApp/wwwroot/js/scripts.js
@@ -12,3 +12,14 @@ var ResetFilePicker = function () {
     document.getElementById("blazIF1").value = "";    
 
 };
+
+window.setImage = async (imageElementId, imageStream) => {
+    const arrayBuffer = await imageStream.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+    const image = document.getElementById(imageElementId);
+    image.onload = () => {
+      URL.revokeObjectURL(url);
+    }
+    image.src = url;
+  }
