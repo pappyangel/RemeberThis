b13669f60456ff55efac0baf484e8443d0ebb29c
Tim Roberts <timrobertsusa@pluralpoint.com>
2023-03-07 21:52:41 +0000

    ticks and milliseconds POC

--- BlazorWebApp/Pages/ReadRTOneItemBase.cs
+++ BlazorWebApp/Pages/ReadRTOneItemBase.cs
@@ -10,9 +10,10 @@ namespace BlazorWebApp.Pages;
 
     public class ReadRTOneItemBase : ComponentBase
     {
-        
+        protected long myTicks = DateTime.Now.Ticks;
+        protected string myTicksString = string.Empty;
         protected string? InfoMsg { get; set; } = "Read All Start Message";
-
+       
         // pre-build item in dev mode so we don;t have to type one in each time we test
         
         // one item from samlle data Create a list of objects - mock data initially
@@ -26,12 +27,30 @@ namespace BlazorWebApp.Pages;
                 rtUserName = "Cosmo-1001",
                 rtDescription = "fun time digging hole for bone",
                 rtLocation = "backyard",
+            
+            
+            // rtLocation = string.Format("{0:10}_{1}{2}",DateTime.Now.Ticks,"zzz","xxx"),
+
                 rtDateTime = DateTime.UtcNow
             };
 
+    
+     protected override void OnInitialized()
+        {
+           
+           long milliseconds = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+           myTicksString = milliseconds.ToString();
+          DateTime dt = DateTime.Now;
+          int ms = dt.Millisecond;
+          myTicksString = ms.ToString();
+          myTicksString = string.Format("{0:MM/dd/yyyy H:mm:ss.fff}",dt);
+        }
+    
     }
 
 public class RTOneItem
 {
 
 }
+
+
