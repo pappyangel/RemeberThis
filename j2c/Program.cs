66507c452d3c9a79a6c277c76a4155d80a45f089
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2023-03-30 12:55:37 +0000

    removed BlazBS from launch

--- j2c/Program.cs
+++ j2c/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddControllersWithViews()
 builder.Services.AddAuthorization(options =>
 {
     // By default, all incoming requests will be authorized according to the default policy
-    options.FallbackPolicy = options.OpenIdConnectDefaults;
+    // options.FallbackPolicy = options.OpenIdConnectDefaults;
     // options.FallbackPolicy = options.DefaultPolicy;
 });
 
