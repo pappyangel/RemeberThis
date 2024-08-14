90f843834da771aa87c7e4b1e5d71e4db917db39
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2024-08-14 17:21:46 +0000

    chore: Update target framework to .NET 8.0

---
+++ rtFrontEnd/Components/Pages/CounterBaseBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace rtFrontEnd.Components.Pages
+{
+    public class CounterBaseBase : ComponentBase
+    {
+        protected int bCount = 0;
+
+        protected void IncrementCount()
+        {
+            bCount++;
+        }
+    }
+}
---
+++ rtFrontEnd/Components/Pages/CounterPartial.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace rtFrontEnd.Components.Pages;
+
+public partial class CounterPartial 
+{
+    private int pCount = 0;
+
+    private void IncrementCount()
+    {
+        pCount++;
+    }
+}
+
