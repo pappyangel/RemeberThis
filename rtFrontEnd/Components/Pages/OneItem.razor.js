05437d8f4972a458f57d689df58ef12367db37db
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2024-08-20 21:06:48 +0000

    feat: Add lazy loading for image preview in OneItem page

--- rtFrontEnd/Components/Pages/OneItem.razor.js
+++ rtFrontEnd/Components/Pages/OneItem.razor.js
@@ -1,5 +1,6 @@
 
-var loadFileJS = function () {
+// function to load the image file into the image element
+export function loadFileJS() {
 
     var image = document.getElementById('pageimage');
     var selectedFile = document.getElementById('blazIF1').files[0];
@@ -7,12 +8,14 @@ var loadFileJS = function () {
 
 };
 
-var ResetFilePicker = function () {
+//  function to reset the file picker if an invalid file was originally chosen
+export function ResetFilePicker() {
 
     document.getElementById("blazIF1").value = "";
 
 };
 
+// unknow if setImage function is needed
 window.setImage = async (imageElementId, imageStream) => {
     const arrayBuffer = await imageStream.arrayBuffer();
     const blob = new Blob([arrayBuffer]);
@@ -22,4 +25,9 @@ window.setImage = async (imageElementId, imageStream) => {
         URL.revokeObjectURL(url);
     }
     image.src = url;
+}
+
+//  function to display message as test
+export function displayMessage(message) {
+    alert('This was your message:' + message);
 }
